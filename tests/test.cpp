c5111afbf2c29feb5623f227d6a58e5045e1d484
Lukyanova-Angelina <st128743@student.spbu.ru>
2025-09-18 02:51:43 +0300

    added 'test'

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,4 +1,10 @@
 /* Lukyanova Angelina st128743@student.spbu.ru
  * Lab Work 2 - Game
  */
+#include "gtest/gtest.h"
 
+int main(int argc, char** argv)
+{
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
